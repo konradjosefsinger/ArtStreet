720c1f12b20b7b07ce00553ba71aabec80eccade
konradjosefsinger <konradjosefsinger@gmail.com>
2023-08-26 19:08:02 +0200

    ready to upload

--- database/index.js
+++ database/index.js
@@ -4,6 +4,7 @@ require('dotenv').config({ path: path.resolve(__dirname, "../.env") });
 const mongoose = require('mongoose');
 
 const connectionString = process.env.DB_CONNECTION_STRING;
+console.log(connectionString)
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
